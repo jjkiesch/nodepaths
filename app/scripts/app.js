b68c0dfad9a51a9220746db6ea09f64062782cbf
Josh Kieschnick <jjkiesch@gmail.com>
2014-12-08 21:11:38 -0600

    Iinitial commit

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name nodepathsApp
+ * @description
+ * # nodepathsApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('nodepathsApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name nodepathsApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the nodepathsApp
+ */
+angular.module('nodepathsApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
